cdc0031f4871381d8974d05c179e22474169c6a7
Charlie Taniere <charlie.taniere@gmail.com>
2022-05-04 16:48:21 +0200

    font awsome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faAt, faGlobe, faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
-  return <footer className="footer"></footer>
+  return (
+    <footer className="footer">
+      <FontAwesomeIcon
+        icon={faTwitter}
+        style={{ fontSize: 30, color: 'white', marginRight: 30 }}
+      />
+      <FontAwesomeIcon
+        icon={faFacebook}
+        style={{ fontSize: 30, color: 'white', marginRight: 30 }}
+      />
+      <FontAwesomeIcon
+        icon={faAt}
+        style={{ fontSize: 30, color: 'white', marginRight: 30 }}
+      />
+      <FontAwesomeIcon
+        icon={faGlobe}
+        style={{ fontSize: 30, color: 'white', marginRight: 30 }}
+      />
+      <FontAwesomeIcon
+        icon={faPhoneFlip}
+        style={{ fontSize: 30, color: 'white', marginRight: 30 }}
+      />
+    </footer>
+  )
 }
 
 export default Footer
